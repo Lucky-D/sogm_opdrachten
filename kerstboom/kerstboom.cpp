e17d2808061812beb33f6b9eee8843534f98ff5b
Lucas Debets <LucasDebets@mt121.hku.nl>
2013-12-03 13:25:00 +0100

    Changed kerstboom, zit nog wel een bug in

--- kerstboom/kerstboom.cpp
+++ kerstboom/kerstboom.cpp
@@ -2,4 +2,21 @@
 
 using namespave std;
 
-std::cout <<"Iets" << std::endl;
+int main ()
+{
+unsigned int aantalkerstbomen=0;
+
+  cout << "Hoeveel kerstbomen wil je?" << endl;
+  cin >> aantalkerstbomen;
+
+  cout << "Aantal kerstbomen: " << aantalkerstbomen << endl;
+  
+  for(int boom=0;boom<aantalkerstbomen;++boom) {
+    for(int rij=0;rij<HOOGTE;++rij) {
+
+    }
+  }
+
+  return 0;
+}
+
