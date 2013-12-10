03de4d1180f697b2ba27df85f6f0023671e80fcf
Lucas Debets <LucasDebets@MacBookPro5.local>
2013-12-10 18:57:36 +0100

    Scripting is nu af en doet het! :)

---
+++ guitarfx/amplifier.h
@@ -0,0 +1,39 @@
+#include <iostream>
+
+#define BUFFERSIZE 10
+
+using namespace std;
+
+
+class Amplifier
+{
+public:
+  Amplifier(); // constructor
+  void show_level();
+  long level;
+};
+
+Amplifier::Amplifier()
+{
+  this->level=5; // default
+}
+
+
+void Amplifier::show_level()
+{
+  cout << "Amplifier level: " << level << endl;
+}
+
+
+int main()
+{
+Amplifier amp1;
+Amplifier amp2;
+
+  //amp1.level325;
+  //amp2.level=2;
+  amp1.show_level();
+  amp2.show_level();
+
+} // main()
+
